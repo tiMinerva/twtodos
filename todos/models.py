d7aa20e87494f7545dbf05d2161b1daa7020cb75
tiMinerva <noc.minerva@gmail.com>
2023-11-04 01:18:36 -0400

    Completado a aula

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(
+        max_length=100, null=False, blank=False, verbose_name="Descreva a Tarefa"
+    )
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(null=False, blank=False)
+    deadline = models.DateField(
+        null=False, blank=False, verbose_name="Data para finalizar"
+    )
     finished_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
 from .models import Todo
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, View
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from datetime import date
 
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
